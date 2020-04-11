9cfe579b2c3605fa0caa6ac073d848eea5089511
Dan Allen <dan@opendevise.com>
2020-04-11 16:46:28 -0600

    add test to verify CLI option name and value can be delimited by equals sign

--- packages/cli/test/cli-test.js
+++ packages/cli/test/cli-test.js
@@ -412,6 +412,21 @@ describe('cli', function () {
     })
   }).timeout(timeoutOverride)
 
+  it('should allow CLI option name and value to be separated by an equals sign', () => {
+    fs.writeJsonSync(playbookFile, playbookSpec, { spaces: 2 })
+    // Q: how do we assert w/ kapok when there's no output; use promise as workaround
+    // TODO once we have common options, we'll need to be sure they get moved before the default command
+    return new Promise((resolve) =>
+      runAntora('--title=#allthedocs --url=https://docs.example.com --quiet the-site.json').on('exit', resolve)
+    ).then((exitCode) => {
+      expect(exitCode).to.equal(0)
+      expect(ospath.join(absDestDir, 'the-component/1.0/index.html'))
+        .to.be.a.file()
+        .with.contents.that.match(new RegExp('<title>Index Page :: #allthedocs</title>'))
+        .with.contents.that.match(new RegExp('<link rel="canonical" href="https://docs.example.com/[^"]*">'))
+    })
+  }).timeout(timeoutOverride)
+
   it('should use the generator specified by the --generator option', () => {
     const generator = ospath.resolve(FIXTURES_DIR, 'simple-generator')
     fs.writeJsonSync(playbookFile, playbookSpec, { spaces: 2 })
