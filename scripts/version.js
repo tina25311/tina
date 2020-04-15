9ad73c3f7259dceda8b9b0c7ddf17b7667be0994
Dan Allen <dan@opendevise.com>
2020-04-15 16:29:50 -0600

    fix lint and formatting in version.js script [skip ci]

--- scripts/version.js
+++ scripts/version.js
@@ -11,8 +11,14 @@ const CHANGELOG_FILE = path.join(PROJECT_ROOT_DIR, 'CHANGELOG.adoc')
 const COMPONENT_VERSION_DESC = path.join(PROJECT_ROOT_DIR, 'docs/antora.yml')
 
 function updateDocsVersion () {
-  const minorVersion = version.split('.').slice(0, 2).join('.')
-  const prereleaseSuffix = version.split('-').slice(1).join('-')
+  const minorVersion = version
+    .split('.')
+    .slice(0, 2)
+    .join('.')
+  const prereleaseSuffix = version
+    .split('-')
+    .slice(1)
+    .join('-')
   return promisify(fs.readFile)(COMPONENT_VERSION_DESC, 'utf8')
     .then((desc) =>
       promisify(fs.writeFile)(
@@ -22,20 +28,18 @@ function updateDocsVersion () {
           .replace(/^prerelease: \S+$/m, `prerelease: ${prereleaseSuffix ? q('-' + prereleaseSuffix) : 'false'}`),
         'utf8'
       )
-    ).then(() =>
-      promisify(exec)('git add docs/antora.yml', { cwd: PROJECT_ROOT_DIR })
     )
+    .then(() => promisify(exec)('git add docs/antora.yml', { cwd: PROJECT_ROOT_DIR }))
 }
 
-function updateChangelog() {
+function updateChangelog () {
   const now = new Date()
   const currentDate = new Date(now - now.getTimezoneOffset() * 60000).toISOString().split('T')[0]
   return promisify(fs.readFile)(CHANGELOG_FILE, 'utf8')
     .then((changelog) =>
       promisify(fs.writeFile)(CHANGELOG_FILE, changelog.replace(/^== Unreleased$/m, `== ${version} (${currentDate})`))
-    ).then(() =>
-      promisify(exec)('git add CHANGELOG.adoc', { cwd: PROJECT_ROOT_DIR })
     )
+    .then(() => promisify(exec)('git add CHANGELOG.adoc', { cwd: PROJECT_ROOT_DIR }))
 }
 
 function q (str) {
