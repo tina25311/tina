09a257e36250b00ad58db830b06a3bec32b67720
Dan Allen <dan@opendevise.com>
2020-04-15 15:09:46 -0600

    update docs version during release [skip ci]

--- scripts/version.js
+++ scripts/version.js
@@ -8,11 +8,41 @@ const { version } = require('../lerna.json')
 
 const PROJECT_ROOT_DIR = path.join(__dirname, '..')
 const CHANGELOG_FILE = path.join(PROJECT_ROOT_DIR, 'CHANGELOG.adoc')
-;(async () => {
+const COMPONENT_VERSION_DESC = path.join(PROJECT_ROOT_DIR, 'docs/antora.yml')
+
+function updateDocsVersion () {
+  const minorVersion = version.split('.').slice(0, 2).join('.')
+  const prereleaseSuffix = version.split('-').slice(1).join('-')
+  return promisify(fs.readFile)(COMPONENT_VERSION_DESC, 'utf8')
+    .then((desc) =>
+      promisify(fs.writeFile)(
+        COMPONENT_VERSION_DESC,
+        desc
+          .replace(/^version: \S+$/m, `version: ${q(minorVersion)}`)
+          .replace(/^prerelease: \S+$/m, `prerelease: ${prereleaseSuffix ? q('-' + prereleaseSuffix) : 'false'}`),
+        'utf8'
+      )
+    ).then(() =>
+      promisify(exec)('git add docs/antora.yml', { cwd: PROJECT_ROOT_DIR })
+    )
+}
+
+function updateChangelog() {
   const now = new Date()
   const currentDate = new Date(now - now.getTimezoneOffset() * 60000).toISOString().split('T')[0]
-  await promisify(fs.readFile)(CHANGELOG_FILE, 'utf8').then((changelog) =>
-    promisify(fs.writeFile)(CHANGELOG_FILE, changelog.replace(/^== Unreleased$/m, `== ${version} (${currentDate})`))
-  )
-  await promisify(exec)('git add CHANGELOG.adoc', { cwd: PROJECT_ROOT_DIR })
+  return promisify(fs.readFile)(CHANGELOG_FILE, 'utf8')
+    .then((changelog) =>
+      promisify(fs.writeFile)(CHANGELOG_FILE, changelog.replace(/^== Unreleased$/m, `== ${version} (${currentDate})`))
+    ).then(() =>
+      promisify(exec)('git add CHANGELOG.adoc', { cwd: PROJECT_ROOT_DIR })
+    )
+}
+
+function q (str) {
+  return `'${str}'`
+}
+
+;(async () => {
+  await updateDocsVersion()
+  await updateChangelog()
 })()
