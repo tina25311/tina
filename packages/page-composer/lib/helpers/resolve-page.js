3edfcf3a952f81c5acf90329569d1a7fe2914796
Dan Allen <dan@opendevise.com>
2020-04-19 02:51:00 -0600

    remove model property from context before calling resolvePage in resolvePage helper

--- packages/page-composer/lib/helpers/resolve-page.js
+++ packages/page-composer/lib/helpers/resolve-page.js
@@ -5,10 +5,7 @@ const { buildPageUiModel } = require('./../build-ui-model')
 module.exports = (spec, { data, hash: context }) => {
   if (!spec) return
   const { contentCatalog, site } = data.root
+  const raw = 'model' in context && (context.model ? !delete context.model : delete context.model)
   const page = contentCatalog.resolvePage(spec, context)
-  if (page) {
-    return 'model' in context && (context.model ? !delete context.model : delete context.model)
-      ? page
-      : buildPageUiModel(site, page, contentCatalog)
-  }
+  if (page) return raw ? page : buildPageUiModel(site, page, contentCatalog)
 }
