6505768cddb43bd3166cd80b11af600fef6cc464
Dan Allen <dan@opendevise.com>
2021-06-28 03:48:13 -0600

    resolves #793 ignore backup files in supplemental UI and UI bundle loaded from directory

--- packages/ui-loader/lib/constants.js
+++ packages/ui-loader/lib/constants.js
@@ -3,6 +3,6 @@
 module.exports = Object.freeze({
   UI_CACHE_FOLDER: 'ui',
   UI_DESC_FILENAME: 'ui.yml',
-  UI_SRC_GLOB: '**/*',
+  UI_SRC_GLOB: '**/*[!~]',
   UI_SRC_OPTS: { follow: true, nomount: true, nosort: true, nounique: true, removeBOM: false, uniqueBy: (m) => m },
 })
