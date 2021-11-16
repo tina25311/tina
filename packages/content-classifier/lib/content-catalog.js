a5d3538ad0a548e8d7600a8ab56b526624a85b05
Dan Allen <dan@opendevise.com>
2021-11-16 02:28:30 -0700

    update API docs for ContentCatalog#resolvePage [skip ci]
    - clarify that the method continues the search for an alias if the page cannot be found

--- packages/content-classifier/lib/content-catalog.js
+++ packages/content-classifier/lib/content-catalog.js
@@ -368,9 +368,10 @@ class ContentCatalog {
    *
    * Parses the specified contextual page ID spec into a page ID object, then attempts to lookup a
    * file with this page ID in the catalog. If a component is specified, but not a version, the
-   * latest version of the component stored in the catalog is used. If a file cannot be resolved,
-   * the function returns undefined. If the spec does not match the page ID syntax, this function
-   * throws an error.
+   * latest version of the component stored in the catalog is used. If a page cannot be resolved,
+   * the search is attempted again for an "alias". If neither a page or alias can be resolved, the
+   * function returns undefined. If the spec does not match the page ID syntax, this function throws
+   * an error.
    *
    * @param {String} spec - The contextual page ID spec (e.g., version@component:module:topic/page.adoc).
    * @param {ContentCatalog} catalog - The content catalog in which to resolve the page file.
