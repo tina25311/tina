2b10be997cafd865c70aaeb47dba2e088b57aee3
Dan Allen <dan@opendevise.com>
2021-12-05 04:43:45 -0700

    git package name from package.json in revert local release script

--- scripts/revert-local-release.sh
+++ scripts/revert-local-release.sh
@@ -2,7 +2,7 @@
 
 for package in `find packages -mindepth 1 -maxdepth 1 -printf "%f\n"`; do
   if [ "$(node -p "require('./packages/$package/package.json').private == true")" == 'true' ]; then continue; fi
-  npm --registry ${npm_config_registry:-http://localhost:4873} unpublish --force @antora/$package
+  npm --registry ${npm_config_registry:-http://localhost:4873} unpublish --force $(node -p "require('./packages/$package/package.json').name")
 done
 
 cd ${1:-../test-release-to}
