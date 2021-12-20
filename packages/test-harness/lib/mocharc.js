09159dc41345e918acd66463fb26857ae3e30284
Dan Allen <dan@opendevise.com>
2021-12-19 17:22:21 -0700

    configure CI to display coverage summary in MR UI

--- packages/test-harness/lib/mocharc.js
+++ packages/test-harness/lib/mocharc.js
@@ -10,16 +10,22 @@ const config = {
 }
 
 if (process.env.npm_config_watch) config.watch = true
-if (process.env.CI) Object.assign(config, { forbidOnly: true, reporter: 'dot' })
+if (process.env.CI) {
+  Object.assign(config, {
+    forbidOnly: true,
+    reporter: 'xunit',
+    'reporter-option': ['output=reports/tests-xunit.xml'],
+  })
+}
 
 function logCoverageReportPath () {
   if (!process.env.NYC_PROCESS_ID) return
   const { CI_PROJECT_PATH, CI_JOB_ID } = process.env
-  const coverageReportRelpath = 'coverage/lcov-report/index.html'
-  const coverageReportPath = CI_JOB_ID
+  const coverageReportRelpath = 'reports/lcov-report/index.html'
+  const coverageReportURL = CI_JOB_ID
     ? `https://gitlab.com/${CI_PROJECT_PATH}/-/jobs/${CI_JOB_ID}/artifacts/file/${coverageReportRelpath}`
     : require('url').pathToFileURL(coverageReportRelpath)
-  console.log(`Coverage report: ${coverageReportPath}`)
+  console.log(`Coverage report: ${coverageReportURL}`)
 }
 
 function resolveSpec () {
