5def973be4e28621f8ba96f74e94b80e6dc0740f
Dan Allen <dan@opendevise.com>
2021-12-19 17:09:13 -0700

    consolidate code in flattenDeep helper

--- packages/content-aggregator/lib/flatten-deep.js
+++ packages/content-aggregator/lib/flatten-deep.js
@@ -2,9 +2,7 @@
 
 function flattenDeep (array, accum = []) {
   const len = array.length
-  for (let i = 0, it; i < len; i++) {
-    Array.isArray((it = array[i])) ? flattenDeep(it, accum) : accum.push(it)
-  }
+  for (let i = 0, it; i < len; i++) Array.isArray((it = array[i])) ? flattenDeep(it, accum) : accum.push(it)
   return accum
 }
 
