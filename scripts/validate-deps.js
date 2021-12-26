a9bafc748360ad17ab4aa13607b1272ec7477e24
Dan Allen <dan@opendevise.com>
2021-12-26 04:02:18 -0700

    validate package dependencies

---
+++ scripts/validate-deps.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const { promises: fsp } = require('fs')
+const { sep: FILE_SEPARATOR } = require('path')
+const builtinModules = require('module').builtinModules.filter((it) => !it.startsWith('_'))
+
+;(async () => {
+  for await (const { name: packageName } of await fsp.opendir('packages')) {
+    if (packageName === 'antora') continue // TODO do something better here
+    const packageDir = `packages${FILE_SEPARATOR}${packageName}`
+    const pkg = require(`..${FILE_SEPARATOR}${packageDir}${FILE_SEPARATOR}package.json`)
+    const prodDeps = Object.keys(pkg.dependencies || {})
+    if (packageName === 'test-harness') prodDeps.push('@antora/logger', '@antora/content-classifier', 'mocha')
+    const devDeps = Object.keys(pkg.devDependencies || {})
+    const deps = [...prodDeps, ...devDeps, `@antora/${packageName}`, '@antora/test-harness']
+    for (const scope of ['lib', 'test']) {
+      const requests = []
+      const dirs = [`${packageDir}${FILE_SEPARATOR}${scope}`]
+      let i = 0
+      while (i < dirs.length) {
+        const dir = dirs[i]
+        for await (const dirent of await fsp.opendir(dir).catch(() => [])) {
+          const name = dirent.name
+          if (dirent.isDirectory()) {
+            dirs.push(`${dir}${FILE_SEPARATOR}${name}`)
+          } else if (name.endsWith('.js')) {
+            await fsp.readFile(`${dir}${FILE_SEPARATOR}${name}`, 'utf8').then((contents) => {
+              for (const [, request] of new Set(contents.matchAll(/require\('(.+?)'\)/g))) {
+                if (request.startsWith('.') || ~builtinModules.indexOf(request)) continue
+                const deepIdx = request.indexOf('/', request.startsWith('@') ? request.indexOf('/') + 1 : 0)
+                requests.push(~deepIdx ? request.substr(0, deepIdx) : request)
+              }
+            })
+          }
+        }
+        i++
+      }
+      const runtimeDeps = scope === 'lib' ? prodDeps : deps
+      const uniqueRequests = [...new Set(requests)]
+      for (const request of uniqueRequests) {
+        if (!~runtimeDeps.indexOf(request)) reportError(`missing ${request} in ${pkg.name} (${scope})`)
+      }
+      const scopedDeps = scope === 'lib' ? prodDeps : devDeps
+      const unused = scopedDeps.reduce((accum, dep) => {
+        if (~uniqueRequests.indexOf(dep)) accum.delete(dep)
+        return accum
+      }, new Set(scopedDeps))
+      if (unused.size) {
+        for (const dep of unused) reportError(`unused ${dep} in ${pkg.name} (${scope})`)
+      }
+    }
+  }
+})()
+
+function reportError (msg) {
+  process.exitCode = 1
+  console.error(msg)
+}
