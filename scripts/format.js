cfddf46fa88aff3eade0cac6e361357ffe7b7241
Dan Allen <dan@opendevise.com>
2021-12-29 01:56:05 -0700

    simplify format script using for await

--- scripts/format.js
+++ scripts/format.js
@@ -4,39 +4,30 @@ const { promises: fsp } = require('fs')
 const ospath = require('path')
 const format = require('prettier-eslint')
 
-function flattenDeep (array, accum = []) {
-  const len = array.length
-  for (let i = 0, it; i < len; i++) Array.isArray((it = array[i])) ? flattenDeep(it, accum) : accum.push(it)
-  return accum
-}
-
-function formatAll (dirs, ignores, cwd = process.cwd()) {
-  return dirs.reduce(
-    (accum, dir) =>
-      fsp.readdir((dir = ospath.join(cwd, dir)), { withFileTypes: true }).then((dirents) => {
-        const subdirs = []
-        const jsfiles = []
-        for (const dirent of dirents) {
-          const name = dirent.name
-          dirent.isDirectory()
-            ? name !== 'node_modules' && subdirs.push(name)
-            : name.endsWith('.js') && jsfiles.push(name)
-        }
-        const promises = subdirs.length ? [formatAll(subdirs, ignores, dir)] : []
-        for (const jsfile of jsfiles) {
-          const filePath = ospath.join(dir, jsfile)
-          if (~ignores.indexOf(filePath)) continue
-          promises.push(
-            fsp.readFile(filePath, 'utf8').then((text) => {
-              const result = format({ text, filePath })
-              return result === text ? false : fsp.writeFile(filePath, result).then(() => true)
+async function formatAll (dirs, ignores, cwd = process.cwd()) {
+  const result = []
+  for (const dir of dirs) {
+    const subdirs = []
+    const absdir = ospath.join(cwd, dir)
+    for await (const dirent of await fsp.opendir(absdir)) {
+      const name = dirent.name
+      if (dirent.isDirectory()) {
+        if (name !== 'node_modules') subdirs.push(name)
+      } else if (name.endsWith('.js')) {
+        const filePath = ospath.join(absdir, name)
+        if (!~ignores.indexOf(filePath)) {
+          result.push(
+            await fsp.readFile(filePath, 'utf8').then((text) => {
+              const formatted = format({ text, filePath })
+              return formatted === text ? false : fsp.writeFile(filePath, formatted).then(() => true)
             })
           )
         }
-        return Promise.all(promises).then((resolved) => accum.then((accumResolved) => [...accumResolved, ...resolved]))
-      }),
-    Promise.resolve([])
-  )
+      }
+    }
+    if (subdirs.length) result.push.apply(result, await formatAll(subdirs, ignores, absdir))
+  }
+  return result
 }
 
 ;(async (dirlist) => {
@@ -48,13 +39,12 @@ function formatAll (dirs, ignores, cwd = process.cwd()) {
       .map((it) => ospath.join(cwd, it))
   )
   await formatAll(dirlist.split(','), ignores, cwd).then((result) => {
-    if (process.env.npm_config_loglevel !== 'silent') {
-      const total = (result = flattenDeep(result)).length
-      const changed = result.filter((it) => it).length
-      const unchanged = total - changed
-      const changedStatus = `changed ${changed} file${changed === 1 ? '' : 's'}`
-      const unchangedStatus = `left ${unchanged} file${unchanged === 1 ? '' : 's'} unchanged`
-      console.log(`prettier-eslint ${changedStatus} and ${unchangedStatus}`)
-    }
+    if (process.env.npm_config_loglevel === 'silent') return
+    const total = result.length
+    const changed = result.filter((it) => it).length
+    const unchanged = total - changed
+    const changedStatus = `changed ${changed} file${changed === 1 ? '' : 's'}`
+    const unchangedStatus = `left ${unchanged} file${unchanged === 1 ? '' : 's'} unchanged`
+    console.log(`prettier-eslint ${changedStatus} and ${unchangedStatus}`)
   })
 })(process.argv[2] || '')
