17d5d755ec96eb37fbc8014a7a1502fc2064a57d
Dan Allen <dan@opendevise.com>
2022-02-11 16:41:53 -0700

    update example of extension that excludes private content source so it preserves value of env key

--- docs/modules/extend/examples/exclude-private-content-sources-extension.js
+++ docs/modules/extend/examples/exclude-private-content-sources-extension.js
@@ -1,7 +1,9 @@
 module.exports.register = function () {
   this.on('playbookBuilt', function ({ playbook }) {
+    const env = playbook.env
     playbook = JSON.parse(JSON.stringify(playbook))
-    playbook.content.sources = playbook.content.sources.filter((source) => !source.url.startsWith('git@'))
+    playbook.content.sources = playbook.content.sources.filter(({ url }) => !url.startsWith('https://git@'))
+    playbook.env = env
     this.updateVariables({ playbook })
   })
 }
