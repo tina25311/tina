6eb6d6d775a7917fcaf54887b7bfa45298b6d11b
Dan Allen <dan@opendevise.com>
2022-08-11 03:09:12 -0600

    simplify code in collateAsciiDocAttributes function

--- packages/asciidoc-loader/lib/util/collate-asciidoc-attributes.js
+++ packages/asciidoc-loader/lib/util/collate-asciidoc-attributes.js
@@ -13,38 +13,35 @@ function collateAsciiDocAttributes (scoped, { initial, merge, mdc }) {
     })
   }
   let changed
-  const collated = Object.entries(scoped).reduce(
-    (accum, [name, val]) => {
-      if (locked[name]) return accum
-      if (val && val.constructor === String) {
-        let alias
-        val = val.replace(ATTR_REF_RX, (ref, refname) => {
-          if (ref.charAt() === '\\') return ref.substr(1)
-          const refval = accum[refname]
-          if (refval == null || refval === false) {
-            if (refname in accum && ref === val) {
-              alias = refval
-            } else if (accum['attribute-missing'] === 'warn') {
-              logger.warn(mdc, "Skipping reference to missing attribute '%s' in value of '%s' attribute", refname, name)
-            }
-            return ref
-          } else if (refval.constructor === String) {
-            const lastIdx = refval.length - 1
-            return refval.charAt(lastIdx) === '@' ? refval.substr(0, lastIdx) : refval
-          } else if (ref === val) {
+  const collated = merge ? Object.assign({}, initial) : initial || {}
+  Object.entries(scoped).forEach(([name, val]) => {
+    if (locked[name]) return collated
+    if (val && val.constructor === String) {
+      let alias
+      val = val.replace(ATTR_REF_RX, (ref, refname) => {
+        if (ref.charAt() === '\\') return ref.substr(1)
+        const refval = collated[refname]
+        if (refval == null || refval === false) {
+          if (refname in collated && ref === val) {
             alias = refval
-            return ref
+          } else if (collated['attribute-missing'] === 'warn') {
+            logger.warn(mdc, "Skipping reference to missing attribute '%s' in value of '%s' attribute", refname, name)
           }
-          return refval.toString()
-        })
-        if (alias !== undefined) val = alias
-      }
-      accum[name] = val
-      changed = true
-      return accum
-    },
-    merge ? Object.assign({}, initial) : initial || {}
-  )
+          return ref
+        } else if (refval.constructor === String) {
+          const lastIdx = refval.length - 1
+          return refval.charAt(lastIdx) === '@' ? refval.substr(0, lastIdx) : refval
+        } else if (ref === val) {
+          alias = refval
+          return ref
+        }
+        return refval.toString()
+      })
+      if (alias !== undefined) val = alias
+    }
+    collated[name] = val
+    changed = true
+  })
   return merge && !changed ? initial : collated
 }
 
