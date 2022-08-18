d57c7c8c6013a1ce3b5b9cd2d3c38da4426347fb
Dan Allen <dan@opendevise.com>
2022-08-18 17:11:23 -0600

    refactor logic in computeOrigin function

--- packages/content-aggregator/lib/aggregate-content.js
+++ packages/content-aggregator/lib/aggregate-content.js
@@ -716,36 +716,32 @@ function loadComponentDescriptor (files, ref, version) {
 
 function computeOrigin (url, authStatus, gitdir, ref, startPath, worktreePath = undefined, editUrl = true) {
   const { shortname: refname, oid: refhash, remote, type: reftype } = ref
-  const origin = { type: 'git', url, gitdir, refname, [reftype]: refname, startPath }
-  if (authStatus) origin.private = authStatus
-  if (worktreePath === undefined) {
-    origin.refhash = refhash
-  } else {
-    if (worktreePath) {
+  const origin = { type: 'git', url, gitdir, refname, [reftype]: refname, refhash, startPath }
+  if (worktreePath !== undefined) {
+    if ((origin.worktree = worktreePath)) {
+      delete origin.refhash
       origin.fileUriPattern =
         (posixify ? 'file:///' + posixify(worktreePath) : 'file://' + worktreePath) +
         (startPath ? '/' + startPath + '/%s' : '/%s')
-    } else {
-      origin.refhash = refhash
-      if (remote) origin.remote = remote
+    } else if (remote) {
+      origin.remote = remote
     }
-    origin.worktree = worktreePath
     if (url.startsWith('file://')) url = undefined
   }
+  if (authStatus) origin.private = authStatus
   if (url) origin.webUrl = url.replace(GIT_SUFFIX_RX, '')
   if (editUrl === true) {
-    let match
-    if (url && (match = url.match(HOSTED_GIT_REPO_RX))) {
+    const match = url && url.match(HOSTED_GIT_REPO_RX)
+    if (match) {
       const host = match[1]
-      let action
+      let action = 'blob'
       let category = ''
       if (host === 'pagure.io') {
-        action = 'blob'
         category = 'f'
       } else if (host === 'bitbucket.org') {
         action = 'src'
-      } else {
-        action = reftype === 'branch' ? 'edit' : 'blob'
+      } else if (reftype === 'branch') {
+        action = 'edit'
       }
       origin.editUrlPattern = 'https://' + path.join(match[1], match[2], action, refname, category, startPath, '%s')
     }
