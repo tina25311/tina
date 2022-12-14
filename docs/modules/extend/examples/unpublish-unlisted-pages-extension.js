9c186439d5de3b2a88097b6bf9bf89265ecd9c00
Dan Allen <dan@opendevise.com>
2022-12-14 16:09:39 -0700

    add use case of unpublishing page not listed in nav

---
+++ docs/modules/extend/examples/unpublish-unlisted-pages-extension.js
@@ -0,0 +1,26 @@
+module.exports.register = function ({ config }) {
+  this
+    .on('navigationBuilt', ({ contentCatalog }) => {
+      contentCatalog.getComponents().forEach(({ versions }) => {
+        versions.forEach(({ name: component, version, navigation: nav, url: defaultUrl }) => {
+          const navEntriesByUrl = getNavEntriesByUrl(nav)
+          const unlistedPages = contentCatalog
+            .findBy({ component, version, family: 'page' })
+            .filter((page) => page.out)
+            .reduce((collector, page) => {
+              if ((page.pub.url in navEntriesByUrl) || page.pub.url === defaultUrl) return collector
+              return collector.concat(page)
+            }, [])
+          if (unlistedPages.length) unlistedPages.forEach((page) => delete page.out)
+        })
+      })
+    })
+}
+
+function getNavEntriesByUrl (items = [], accum = {}) {
+  items.forEach((item) => {
+    if (item.urlType === 'internal') accum[item.url.split('#')[0]] = item
+    getNavEntriesByUrl(item.items, accum)
+  })
+  return accum
+}
