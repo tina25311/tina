36d38a4ec79e56b47a5826023b5f9a3eec207024
Jake Cahill <jake@redpanda.com>
2023-06-06 09:32:54 +0000

    add extension use case to docs for resolving attribute references in attachments

---
+++ docs/modules/extend/examples/resolve-attribute-references-in-attachments.js
@@ -0,0 +1,29 @@
+module.exports.register = function () {
+  this.on('contentClassified', ({ contentCatalog }) => {
+    const componentVersionTable = contentCatalog.getComponents().reduce((componentMap, component) => {
+      componentMap[component.name] = component.versions.reduce((versionMap, componentVersion) => {
+        versionMap[componentVersion.version] = componentVersion
+        return versionMap
+      }, {})
+      return componentMap
+    }, {})
+    contentCatalog.findBy({ family: 'attachment' }).forEach((attachment) => {
+      const componentVersion = componentVersionTable[attachment.src.component][attachment.src.version]
+      let attributes = componentVersion.asciidoc?.attributes
+      if (!attributes) return
+      attributes = Object.entries(attributes).reduce((accum, [name, val]) => {
+        accum[name] = val && val.endsWith('@') ? val.slice(0, val.length - 1) : val
+        return accum
+      }, {})
+      let modified
+      const result = attachment.contents.toString().replace(/\{([\p{Alpha}\d_][\p{Alpha}\d_-]*)\}/gu, (match, name) => {
+        if (!(name in attributes)) return match
+        modified = true
+        let value = attributes[name]
+        if (value.endsWith('@')) value = value.slice(0, value.length - 1)
+        return value
+      })
+      if (modified) attachment.contents = Buffer.from(result)
+    })
+  })
+}
