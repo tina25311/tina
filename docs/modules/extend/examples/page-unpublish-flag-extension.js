fc812fa82755ca2ea5319f6ac50312bc10320331
Dan Allen <dan@opendevise.com>
2023-10-04 02:16:58 -0600

    add extension use case to unpublish pages flagged with page-unpublish page attribute

---
+++ docs/modules/extend/examples/page-unpublish-flag-extension.js
@@ -0,0 +1,7 @@
+module.exports.register = function () {
+  this.on('documentsConverted', ({ contentCatalog }) => {
+    contentCatalog.getPages((page) => page.out).forEach((page) => {
+      if (page.asciidoc?.attributes['page-unpublish'] != null) delete page.out
+    })
+  })
+}
