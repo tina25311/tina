e96a56dd983b074cb447b94d503b812490798a13
Dan Allen <dan@opendevise.com>
2023-10-26 18:02:31 -0600

    add doc-to-pdf extension to extension use cases

---
+++ docs/modules/extend/examples/doc-to-pdf-extension.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const fsp = require('node:fs/promises')
+const ospath = require('node:path')
+const { posix: path } = ospath
+const { execFile } = require('node:child_process')
+
+module.exports.register = function () {
+  this.on('contentClassified', async ({ playbook, contentCatalog }) => {
+    const docExtnames = { '.docx': true, '.fodt': true }
+    const filesToConvert = contentCatalog.getFiles().filter(({ src } ) => src.family == 'attachment' && docExtnames[src.extname])
+    if (!filesToConvert.length) return
+    const buildDirBase = ospath.join(playbook.dir, 'build/doc-to-pdf')
+    const convertArgs = ['--writer', '--convert-to', 'pdf']
+    const convertOpts = { cwd: buildDirBase, windowsHide: true }
+    try {
+      await fsp.mkdir(buildDirBase, { recursive: true })
+      await Promise.all(filesToConvert.map((file) => {
+        const sourceRelpath = `${file.src.component}-${file.src.module}-${file.out.basename}`
+        convertArgs.push(sourceRelpath)
+        return fsp.writeFile(ospath.join(buildDirBase, sourceRelpath), file.contents)
+      }))
+      await new Promise((resolve, reject) => {
+        execFile('libreoffice', convertArgs, convertOpts, (err, stderr, stdout) => {
+          if (!err) return resolve()
+          const splitIdx = stderr.indexOf('Usage: ')
+          if (~splitIdx) stderr = stderr.slice(0, splitIdx).trimEnd()
+          if (stderr) err.message += stderr
+          reject(err)
+        })
+      })
+      await Promise.all(filesToConvert.map((file) => {
+        file.out.path = path.join(file.out.dirname, (file.out.basename = file.out.basename.slice(0, -file.src.extname.length) + '.pdf'))
+        file.pub.url = file.pub.url.slice(0, -file.src.extname.length) + '.pdf'
+        const sourceRelpath = `${file.src.component}-${file.src.module}-${file.out.basename}`
+        return fsp.readFile(ospath.join(buildDirBase, sourceRelpath)).then((contents) => (file.contents = contents))
+      }))
+    } finally {
+      await fsp.rm(buildDirBase, { recursive: true, force: true })
+    }
+  })
+}
