2205bd81e29ed15108153df0b56bb004f7c164a7
Dan Allen <dan@opendevise.com>
2024-01-11 15:36:16 -0700

    add missing include to docs

---
+++ docs/modules/extend/examples/set-global-asciidoc-attributes-extension.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports.register = function () {
+  this.on('beforeProcess', (({ siteAsciiDocConfig }) => {
+    const buildDate = new Date().toISOString()
+    siteAsciiDocConfig.attributes['build-date'] = buildDate
+  }))
+}
