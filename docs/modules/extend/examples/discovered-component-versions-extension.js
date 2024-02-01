2af0cee00c1a7339636bc4c53e518d2d87a0fabb
Martin <github@diemattels.at>
2024-02-01 10:13:36 +0000

    add file count to discovered-component-versions-extension.js

--- docs/modules/extend/examples/discovered-component-versions-extension.js
+++ docs/modules/extend/examples/discovered-component-versions-extension.js
@@ -2,9 +2,10 @@
 
 module.exports.register = function () {
   this.once('contentAggregated', ({ contentAggregate }) => {
+    console.log('Discovered the following component versions')
     contentAggregate.forEach((bucket) => {
       const sources = bucket.origins.map(({ url, refname }) => ({ url, refname }))
-      console.log({ name: bucket.name, version: bucket.version, sources })
+      console.log({ name: bucket.name, version: bucket.version, files: bucket.files.length, sources })
     })
   })
 }
