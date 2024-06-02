49248f5cd0ae3b50064a00bb759cd70d1275a266
Dan Allen <dan@opendevise.com>
2024-06-02 03:40:51 -0600

    fix code formatting

--- packages/document-converter/lib/convert-documents.js
+++ packages/document-converter/lib/convert-documents.js
@@ -76,9 +76,7 @@ function buildCacheKey ({ component, version }) {
 
 function registerPageAliases (aliases, targetFile, contentCatalog) {
   if (!aliases) return
-  aliases
-    .split(',')
-    .forEach((spec) => (spec = spec.trim()) && contentCatalog.registerPageAlias(spec, targetFile))
+  aliases.split(',').forEach((spec) => (spec = spec.trim()) && contentCatalog.registerPageAlias(spec, targetFile))
 }
 
 function requireAsciiDocLoader () {
