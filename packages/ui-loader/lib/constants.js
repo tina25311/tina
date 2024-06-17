01f5939c2a0a33dcad8b7a44ffa834812dd25110
Dan Allen <dan@opendevise.com>
2024-06-17 14:23:45 -0600

    resolves #1130 replace glob-stream with fast-glob

--- packages/ui-loader/lib/constants.js
+++ packages/ui-loader/lib/constants.js
@@ -3,14 +3,13 @@
 module.exports = Object.freeze({
   UI_CACHE_FOLDER: 'ui',
   UI_DESC_FILENAME: 'ui.yml',
-  UI_SRC_GLOB: '**/*[!~]',
+  UI_SRC_GLOB: '**/!(*~)',
   UI_SRC_OPTS: {
+    braceExpansion: false,
     dot: true,
-    follow: true,
-    ignore: ['.git/**'],
-    nomount: true,
-    nosort: true,
-    nounique: true,
-    strict: false,
+    ignore: ['.git'],
+    objectMode: true,
+    onlyFiles: false,
+    unique: false,
   },
 })
