c6751da8a3b4a71b24320a40ef4614363e503422
Dan Allen <dan@opendevise.com>
2024-06-25 03:47:32 -0600

    upgrade commander

--- packages/cli/lib/cli.js
+++ packages/cli/lib/cli.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const buildPlaybook = require('@antora/playbook-builder')
-const cli = require('./commander')
+const { program: cli } = require('./commander')
 const convict = require('@antora/playbook-builder/lib/solitary-convict') // drop lib segment in Antora 4
 const ospath = require('node:path')
 const userRequire = require('@antora/user-require-helper')
