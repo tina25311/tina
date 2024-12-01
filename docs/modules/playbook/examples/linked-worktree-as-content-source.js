daa94cda5c17a7ec63207820f11b4dec8331f8ad
Dan Allen <dan@opendevise.com>
2024-12-01 12:50:17 -0700

    short circuit logic if dot git is not a file to make it more clear

--- docs/modules/playbook/examples/linked-worktree-as-content-source.js
+++ docs/modules/playbook/examples/linked-worktree-as-content-source.js
@@ -21,25 +21,25 @@ module.exports.register = function () {
       const { url, branches } = contentSource
       if (url.charAt() !== '.') continue
       const absdir = expandPath(url, { dot: playbook.dir })
-      const gitfile = ospath.join(absdir, '.git')
-      if (await fsp.stat(gitfile).then((stat) => !stat.isDirectory(), () => false)) {
-        const worktreeGitdir = await fsp.readFile(gitfile, 'utf8')
-          .then((contents) => contents.substr(8).trimEnd())
-        const worktreeBranch = await fsp.readFile(ospath.join(worktreeGitdir, 'HEAD'), 'utf8')
-          .then((contents) => contents.trimEnd().replace(/^ref: (?:refs\/heads\/)?/, ''))
-        const reldir = ospath.relative(
-          playbook.dir,
-          await fsp.readFile(ospath.join(worktreeGitdir, 'commondir'), 'utf8')
-            .then((contents) => {
-              const gitdir = ospath.join(worktreeGitdir, contents.trimEnd())
-              return ospath.basename(gitdir) === '.git' ? ospath.dirname(gitdir) : gitdir
-            })
-        )
-        contentSource.url = reldir ? `.${ospath.sep}${reldir}` : '.'
-        if (!branches) continue
-        contentSource.branches = (branches.constructor === Array ? branches : [branches])
-          .map((pattern) => pattern.replaceAll('HEAD', worktreeBranch))
-      }
+      const dotgit = ospath.join(absdir, '.git')
+      const dotgitIsFile = await fsp.stat(dotgit).then((stat) => stat.isFile(), () => false)
+      if (!dotgitIsFile) continue
+      const worktreeGitdir = await fsp.readFile(dotgit, 'utf8')
+        .then((contents) => contents.substr(8).trimEnd())
+      const worktreeBranch = await fsp.readFile(ospath.join(worktreeGitdir, 'HEAD'), 'utf8')
+        .then((contents) => contents.trimEnd().replace(/^ref: (?:refs\/heads\/)?/, ''))
+      const reldir = ospath.relative(
+        playbook.dir,
+        await fsp.readFile(ospath.join(worktreeGitdir, 'commondir'), 'utf8')
+          .then((contents) => {
+            const gitdir = ospath.join(worktreeGitdir, contents.trimEnd())
+            return ospath.basename(gitdir) === '.git' ? ospath.dirname(gitdir) : gitdir
+          })
+      )
+      contentSource.url = reldir ? `.${ospath.sep}${reldir}` : '.'
+      if (!branches) continue
+      contentSource.branches = (branches.constructor === Array ? branches : [branches])
+        .map((pattern) => pattern.replaceAll('HEAD', worktreeBranch))
     }
   })
 }
